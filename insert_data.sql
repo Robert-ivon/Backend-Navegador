10aadc87bd644d1eef932619da2304eb45fac6b0
Robert <160376307+Robert-ivon@users.noreply.github.com>
2024-10-16 17:53:44 -0600

    Add files via upload
    archivo sql de tablas e informacion prueba ainsertar

---
+++ insert_data.sql
@@ -0,0 +1,14 @@
+ -- Instrucciones SQL para insertar datos en la tabla Bookmark
+INSERT INTO Bookmark (url, title, user_id)
+VALUES 
+('https://www.facebook.com', 'Facebook', 1),
+('https://www.instagram.com', 'Instagram', 1),
+('https://www.twitter.com', 'Twitter/X', 1),
+('https://www.linkedin.com', 'LinkedIn', 1),
+('https://www.tiktok.com', 'TikTok', 1),
+('https://www.youtube.com', 'YouTube', 1),
+('https://www.snapchat.com', 'Snapchat', 1),
+('https://www.pinterest.com', 'Pinterest', 1),
+('https://www.reddit.com', 'Reddit', 1),
+('https://www.whatsapp.com', 'WhatsApp', 1);
+
